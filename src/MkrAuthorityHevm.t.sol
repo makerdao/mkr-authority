997a81af6c57567d70c413cfa60f7299866bb7cc
andy8052 <andychorlian@gmail.com>
2019-08-20 10:21:08 -0400

    mkr auth hevm tests failing

--- src/MkrAuthorityHevm.t.sol
+++ src/MkrAuthorityHevm.t.sol
@@ -2,27 +2,43 @@ pragma solidity >=0.5.10;
 
 import "ds-test/test.sol";
 
+import "./MkrAuthority.sol";
+
 interface ERC20 {
     function setAuthority(address whom) external;
     function setOwner(address whom) external;
     function owner() external returns(address);
     function authority() external returns(address);
+    function balanceOf( address who ) external view returns (uint value);
+    function mint(address usr, uint256 wad) external;
+    function burn(address usr, uint256 wad) external;
+    function stop() external;
 }
 
 contract OwnerUpdate is DSTest {
+
     // Test with this:
     // It uses the Multisig as the caller
-    // dapp build && DAPP_TEST_TIMESTAMP=$(seth block latest timestamp) DAPP_TEST_NUMBER=$(seth block latest number) DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc=$ETH_RPC_URL --json-file=out/dapp.sol.json
+    // dapp build
+    // DAPP_TEST_TIMESTAMP=$(seth block latest timestamp) DAPP_TEST_NUMBER=$(seth block latest number) DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7 hevm dapp-test --rpc=$ETH_RPC_URL --json-file=out/dapp.sol.json
     function testChangeOwners() public {
-        address auth = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
+        MkrAuthority authority = new MkrAuthority();
+
+        authority.rely(msg.sender);
+
         ERC20 mkr = ERC20(0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2);
 
-        mkr.setAuthority(auth);
-
-        assertTrue(mkr.authority() == auth);
-
+        mkr.setAuthority(address(authority));
+        assertTrue(address(mkr.authority()) == address(authority));
         mkr.setOwner(address(0));
+        assertTrue(address(mkr.owner()) == address(0));
 
-        assertTrue(mkr.owner() == address(0));
+        uint balance = mkr.balanceOf(msg.sender);
+
+        // mkr.mint(msg.sender, 1);
+        // assertEq(balance + 1, mkr.balanceOf(msg.sender));
+
+        // mkr.burn(address(msg.sender), 1);
+        // assertEq(balance, mkr.balanceOf(msg.sender));
     }
 }
